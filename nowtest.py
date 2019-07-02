c3aa310db284394324f53f25daeb7ad25675fb33
wangzheng62 <363579018@qq.com>
2019-07-02 17:17:18 +0800

    1213333111

--- nowtest.py
+++ nowtest.py
@@ -17,8 +17,15 @@ if __name__=='__main__':
     u2=r'https://www.google.com/flights#flt=LAX.LHR,LGW,CDG,ORY,ARN.2019-09-05*BCN,MAD,FRA,GVA,FCO.LAX.2019-09-24;c:USD;e:1;so:1;sd:1;t:f;tt:m'
     driver.get(u2)
     sleep(10)
-    elems=driver.find_elements_by_xpath('//span[@class="gws-flights__ellipsize"]/span/span')
-    elems1=driver.find_elements_by_class_name('gws-flights__footer-picker-value')
-    print(len(elems))
-    for e in elems:
-        print(e.text)
+    try:
+        elems=driver.find_elements_by_xpath('//span[@class="gws-flights__ellipsize"]/span/span')
+        elems1=driver.find_elements_by_class_name('gws-flights__footer-picker-value')
+        print(len(elems))
+        for e in elems:
+            print(e.text)
+    except:
+        elems = driver.find_elements_by_xpath('//span[@class="gws-flights__ellipsize"]/span/span')
+        elems1 = driver.find_elements_by_class_name('gws-flights__footer-picker-value')
+        print(len(elems))
+        for e in elems:
+            print(e.text)
