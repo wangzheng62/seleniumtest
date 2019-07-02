bc3500e52e275a85bc4d2577ef34523ff37b54da
wangzheng62 <363579018@qq.com>
2019-07-02 17:32:08 +0800

    1213333111

--- nowtest.py
+++ nowtest.py
@@ -16,25 +16,14 @@ if __name__=='__main__':
     u1=r'http://www.w3school.com.cn/xpath/xpath_syntax.asp'
     u2=r'https://www.google.com/flights#flt=LAX.LHR,LGW,CDG,ORY,ARN.2019-09-05*BCN,MAD,FRA,GVA,FCO.LAX.2019-09-24;c:USD;e:1;so:1;sd:1;t:f;tt:m'
     driver.get(u2)
-    with open('1.html','w') as f:
-        f.write(driver.page_source)
-    sleep(10)
-    with open('2.html','w') as f:
-        f.write(driver.page_source)
-    sleep(10)
-    with open('3.html','w') as f:
-        f.write(driver.page_source)
-    '''
-    try:
-        elems=driver.find_elements_by_xpath('//span[@class="gws-flights__ellipsize"]/span/span')
-        elems1=driver.find_elements_by_class_name('gws-flights__footer-picker-value')
-        print(len(elems))
-        for e in elems:
-            print(e.text)
-    except:
-        elems = driver.find_elements_by_xpath('//span[@class="gws-flights__ellipsize"]/span/span')
-        elems1 = driver.find_elements_by_class_name('gws-flights__footer-picker-value')
-        print(len(elems))
-        for e in elems:
-            print(e.text)
-    '''
+
+    elems=driver.find_elements_by_xpath('//span[@class="gws-flights__ellipsize"]/span/span')
+    elems1=driver.find_elements_by_class_name('gws-flights__footer-picker-value')
+    print(len(elems))
+    for e in elems:
+        print(e.text)
+    sleep(5)
+    elems = driver.find_elements_by_xpath('//span[@class="gws-flights__ellipsize"]/span/span')
+    print(len(elems))
+    for e in elems:
+        print(e.text)
