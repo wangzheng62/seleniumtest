109e5e9c4ad9cdd9932073e739a14b655eb55621
wangzheng62 <363579018@qq.com>
2019-07-02 17:12:26 +0800

    1213333111

--- nowtest.py
+++ nowtest.py
@@ -7,6 +7,7 @@ from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from json import dumps
+from time import sleep
 chrome_options = Options()
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-gpu")
@@ -15,6 +16,7 @@ if __name__=='__main__':
     u1=r'http://www.w3school.com.cn/xpath/xpath_syntax.asp'
     u2=r'https://www.google.com/flights#flt=LAX.LHR,LGW,CDG,ORY,ARN.2019-09-05*BCN,MAD,FRA,GVA,FCO.LAX.2019-09-24;c:USD;e:1;so:1;sd:1;t:f;tt:m'
     driver.get(u2)
+    sleep(10)
     elems=driver.find_elements_by_xpath('//span[@class="gws-flights__ellipsize"]/span/span')
     elems1=driver.find_elements_by_class_name('gws-flights__footer-picker-value')
     print(len(elems))
