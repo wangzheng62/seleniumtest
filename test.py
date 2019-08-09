ff13baec28124f76ceab0b71c9b127e3015fd9b8
wangzheng62 <363579018@qq.com>
2019-08-09 15:09:59 +0800

    1213333111

--- test.py
+++ test.py
@@ -1,10 +1,38 @@
-import sys
-from PyQt5.QtWidgets import QApplication,QWidget
-if __name__=='__main__':
-    app=QApplication(sys.argv)
-    w=QWidget()
-    w.resize(480,320)
-    w.move(300,300)
-    w.setWindowTitle('Simple')
-    w.show()
-    sys.exit(app.exec_())
+from openpyxl import Workbook,load_workbook
+from openpyxl.chart import (
+    AreaChart,
+    Reference,
+    Series,
+)
+from tempfile import NamedTemporaryFile
+wb = Workbook()
+ws = wb.active
+
+rows = [
+    ['Number', 'Batch 1', 'Batch 2'],
+    [2, 40, 30],
+    [3, 40, 25],
+    [4, 50, 30],
+    [5, 30, 10],
+    [6, 25, 5],
+    [7, 50, 10],
+]
+
+for row in rows:
+    ws.append(row)
+
+chart = AreaChart()
+chart.title = "Area Chart"
+chart.style = 13
+chart.x_axis.title = 'Test'
+chart.y_axis.title = 'Percentage'
+
+cats = Reference(ws, min_col=1, min_row=1, max_row=7)
+data = Reference(ws, min_col=2, min_row=1, max_col=3, max_row=7)
+chart.add_data(data, titles_from_data=True)
+chart.set_categories(cats)
+
+ws.add_chart(chart, "A10")
+
+wb.save("area.xlsx")
+wb2=load_workbook('123.xlsx')
---
+++ zhaopin.py
@@ -0,0 +1,6 @@
+from classdef import Job,Field,Do
+class chef():
+    pass
+
+if __name__=='__main__':
+    p1='https://sou.zhaopin.com/?jl=719'
