04e7244479c9591671dae05b235b3c5ea16c1c89
wangzheng62 <363579018@qq.com>
2019-08-14 09:52:51 +0800

    1213333111

--- zhaopin.py
+++ zhaopin.py
@@ -2,10 +2,11 @@ from classdef import Job,Field,Do
 from openpyxl import Workbook
 import numpy as np
 class Chef():
-    HREF=Field('//div[@class="contentpile__content__wrapper__item clearfix"]/a','href')
-    JOBNAME=Field('//span[@class="contentpile__content__wrapper__item__info__box__jobname__title"]','text')
-    SARAY=Field('//p[@class="contentpile__content__wrapper__item__info__box__job__saray"]','text')
-    #welfare=Field('//div[@class="contentpile__content__wrapper__item__info__box__welfare job_welfare"]/div','text')
+    wrap=Field('//div[@id="listContent"]/div')
+    href=Field('//div[@class="contentpile__content__wrapper__item clearfix"]/a','href')
+    jobname=Field('//span[@class="contentpile__content__wrapper__item__info__box__jobname__title"]','text')
+    saray=Field('//p[@class="contentpile__content__wrapper__item__info__box__job__saray"]','text')
+    welfare=Field('//div[@class="contentpile__content__wrapper__item__info__box__welfare job_welfare"]/div','text')
 def wt(d,name):
 
 
@@ -26,12 +27,11 @@ if __name__=='__main__':
     p3='https://sou.zhaopin.com/?p={}&jl=719&kw=%E5%8E%A8%E5%B8%88&kt=3&sf=0&st=0'
     j1 = Job()
     dir1 = j1.out(p1, Chef)
-    wt(dir1,1)
+    #wt(dir1,1)
     i=2
-    while(i<=10):
-        p='https://sou.zhaopin.com/?p={}&jl=719&kw=%E5%8E%A8%E5%B8%88&kt=3&sf=0&st=0'.format(i)
-        j=Job()
-        d=j.out(p,Chef)
-        wt(d,i)
-        i=i+1
-
+    #while(i<=10):
+    #    p='https://sou.zhaopin.com/?p={}&jl=719&kw=%E5%8E%A8%E5%B8%88&kt=3&sf=0&st=0'.format(i)
+    #    j=Job()
+    #    d=j.out(p,Chef)
+     #   wt(d,i)
+     #   i=i+1
