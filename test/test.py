6dd8579c28d34cb2ecad11551afea4caea62b9bd
wangzheng62 <363579018@qq.com>
2019-08-21 14:46:56 +0800

    1213333111

--- test/test.py
+++ test/test.py
@@ -1,8 +1,10 @@
-from itertools import count,cycle,repeat,groupby,filterfalse
-print(count(10).__next__())
-print(cycle('abcd').__next__())
-print(repeat(11,5).__next__())
-l=[list(g) for k, g in groupby('AAAABBBCCD')]
-print(l)
-for i in filterfalse(lambda x: x%2, range(10)):
-    print(i)
+import jieba
+seg=jieba.lcut('中国平安人寿保险股份有限公司河南分公司优才之家团队',cut_all=True)
+print(seg)
+fliterkey=['保险','诺万信息','未之来','平安','中科院']
+s=set(seg)
+s1=set(fliterkey)
+print(s)
+print(s1)
+d=s&s1
+print(len(d))
--- usage/zhaopin.py
+++ usage/zhaopin.py
@@ -1,5 +1,6 @@
 from core.classdef import Job,Field
 from core.funcdef import savexls,sav,savejson
+from jieba  import lcut
 class Work():
     wrap=Field('//div[@id="listContent"]/div')
     href=Field('//div[@class="contentpile__content__wrapper__item clearfix"]/a','href')
@@ -7,20 +8,43 @@ class Work():
     jobname=Field('//span[@class="contentpile__content__wrapper__item__info__box__jobname__title"]','text')
     saray=Field('//p[@class="contentpile__content__wrapper__item__info__box__job__saray"]','text')
     welfare=Field('//div[@class="contentpile__content__wrapper__item__info__box__welfare job_welfare"]/div','text')
+class Info():
+    address=Field('//span[@class ="job-address__content-text"]')
+    details=Field('//div[@class ="describtion__detail-content"]/p')
+def fpass():
+    pass
+def fstop():
+    pass
 if __name__=='__main__':
+    ss=['中国平安人寿保险股份有限公司河南分公司优才之家团队']
+    fliterkey=['保险','诺万信息','未之来','平安','中科院']
+    flitersaray=['2','3','4']
+    fliterwelfare=['']
+    for key in fliterkey:
+        if key in ''.join(ss):
+            print('hahaha')
+
+
     #chef
     p1='https://sou.zhaopin.com/?jl=719&kw=%E5%8E%A8%E5%B8%88&kt=3&sf=0&st=0'
     #data
     p01='https://sou.zhaopin.com/?jl=719&sf=0&st=0&kw=%E6%95%B0%E6%8D%AE%E5%88%86%E6%9E%90&kt=3'
+    p02='https://sou.zhaopin.com/?jl=719&sf=0&st=0&kw=%E8%B0%B7%E6%AD%8C&kt=3'
     j1 = Job()
     l1 = j1.out(p01, Work)
+    res=[]
+    res=res+f(l1)
 
-    savexls(l1,1)
     i=2
+
     while(i<=20):
         p='https://sou.zhaopin.com/?p={}&jl=719&sf=0&st=0&kw=%E6%95%B0%E6%8D%AE%E5%88%86%E6%9E%90&kt=3'.format(i)
         j=Job()
-        d=j.out(p,Work)
-        savexls(d,i)
+        try:
+            d=j.out(p,Work)
+            res=res+f(d)
+        except:
+            break
         i=i+1
+    savexls(res,123)
 
