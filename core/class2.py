9480da5c99651ce1eb91dced1a6e1511dd111df3
wangzheng62 <363579018@qq.com>
2020-06-19 16:46:10 +0800

    1213333111

--- core/class2.py
+++ core/class2.py
@@ -22,16 +22,9 @@ class Engine():
         try:
             self.driver.close()
             self.driver.quit()
-        except:
-            self.driver.close()
-            self.driver.quit()
-    def __del__(self):
-        try:
-            self.driver.close()
-            self.driver.quit()
-        except:
-            self.driver.close()
-            self.driver.quit()
+        except Exception as e:
+            print(e)
+
     def __getddata(self,url,data):
         self.driver.get(url)
         res={}
@@ -42,19 +35,53 @@ class Engine():
             else:
                 res[key] = elem.get_attribute(data[key][1])
         return res
+    def singleget(self,url,data):
+        try:
+            con=self.__getddata(url,data)
+            return con
+        except Exception as e:
+            print(e)
+#递归
+    def __recuv(self,url,data):
+        if 'next' not in data:
+            raise ValueError('递归检索数据中必须包含key为"next"，value为url的定义项')
+        n=0
+        while True:
+            print(n)
+            nexturl=yield self.__getddata(url,data)
+            url=nexturl
+            n=n+1
+    def recuv(self,url,data,func=print):
+        r=self.__recuv(url,data)
+        try:
+            con=r.send(None)
+        except Exception as e:
+            print(e)
+            return
+        func(con)
+        while con['next']:
+            try:
+                con=r.send(con['next'])
+                func(con)
+            except Exception as e:
+                print(e)
+                break
+        r.close()
+#树遍历
 
-    def walk(self,urltree,data,save='text'):
-        return self.__getddata(urltree,data)
+def ma(s):
+    pattern=r'[-\(\)\.\d\w\u4e00-\u9fa5]'
+    p=re.compile(pattern)
+    l=p.findall(s)
+    res=''.join(l)
+    return res
 
+def save(con):
+    title=ma(con['title'])
+    with open(title+'.txt','ab') as f:
+        f.write(con['content'].encode(errors='ignore'))
 def engine():
     e=Engine()
     return e
-
-data={'title':('//*[@id="wrapper"]/div[5]/div/div[2]/h1','text'),
-      'content':('//*[@id="content"]','text'),
-      'next':('//*[@id="wrapper"]/div[5]/div/div[4]/a[4]','href')}
-
-p='http://www.shubaoxsw.org/2_2741/296549.htl'
-with engine() as e1:
-    print(e1.walk(p,data))
-
+if __name__=='__main__':
+    pass
